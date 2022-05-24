8f4ee788458b14c86e6611a204ad6ceb19e8710c
Hongda Li <lihd1417@163.com>
2022-05-24 17:09:51 +0800

    set up the nodes and elements information

--- grid.c
+++ grid.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 int main(int argc,char* argv[]){
 	float h;	
@@ -9,34 +10,58 @@ int main(int argc,char* argv[]){
 	}	
 	int i,j;//,k;
 	int length=1;
-	int number=length/h;
-	float x[number+1][number+1];
-	float y[number+1][number+1];
-	for(i=0;i<number+1;i++)
+	int n=length/h;
+	int num_of_elements = n*n;
+	int num_of_nodes=(n+1)*(n+1);
+	//float a=0;
+	//nodes information
+	float nodes[num_of_nodes][3];
+	for (i=0;i<num_of_nodes;i++)
 	{
-		for(j=0;j<number+1;j++)
-		{
-			x[i][j]=0;			
-			x[i][j]=j*h;
-			y[i][j]=0;
-			y[i][j]=i*h;
-		}
+		int a=i/(n+1);
+		nodes[i][0]=i;
+		nodes[i][1]=(i-a*n)*h;
+		nodes[i][2]=a*h;
 	}
-	printf("x[%d][%d] is : \n",number+1,number+1);
-	for(i = 0;i<number+1;i++)
+	//elements information
+	int elements[num_of_elements][5];
+	for (i=0;i<num_of_elements;i++)
 	{
-		for (j=0;j<number+1;j++)
+		int a=i/(n+1);
+		elements[i][0]=i;
+		elements[i][1]=i+a;
+		elements[i][2]=i+a+1;
+		elements[i][3]=i+a+n+1;
+		elements[i][4]=i+a+n+2;
+		
+	}
+	//float x[number+1][number+1];
+	//float y[number+1][number+1];
+	//for(i=0;i<number+1;i++)
+	//{
+	//	for(j=0;j<number+1;j++)
+	//	{
+	//		x[i][j]=0;			
+	//		x[i][j]=j*h;
+	//		y[i][j]=0;
+	//		y[i][j]=i*h;
+	//	}
+	//}
+	printf("nodes[%d][%d] is : \n",n+1,n+1);
+	for(i = 0;i<num_of_nodes;i++)
+	{
+		for (j=0;j<3;j++)
 		{
-			printf("%f\t",x[i][j]);
+			printf("%f\t",nodes[i][j]);
 		}
 		printf("\n");
 	}
-	printf("y[%d][%d] is : \n",number+1,number+1);
-	for(i = 0;i<number+1;i++)
+	printf("elements[%d][%d] is : \n",n,n);
+	for(i = 0;i<num_of_elements;i++)
 	{
-		for (j=0;j<number+1;j++)
+		for (j=0;j<5;j++)
 		{
-			printf("%f\t",y[i][j]);
+			printf("%d\t",elements[i][j]);
 		}
 		printf("\n");
 	}
