09f42016fee35dc387275bb187e2a48203d74f29
Hongda Li <lihd1417@163.com>
2022-05-28 15:32:37 +0800

    petsc

--- grid.c
+++ grid.c
@@ -1,6 +1,10 @@
+static char help[] = "Solves a 10000x10000 linear system";
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include<petscksp.h>
+#include<petscmath.h>
 
 #define PI 3.14159265
 
@@ -20,22 +24,30 @@ void huayifa_G_B(double **G_B, int num_of_elements, int num_of_nodes, int n, dou
 double **mallocMatrix(int row, int col);
 void freeMatrix(double **a);
 
-int main(/*int argc, char *argv[]*/)
+int main(int argc, char **args)
 {
-	double h=0.5;
-	// if (argc == 2)
-	// {
-	// 	h = atof(argv[1]);
-	// }
-	int i = 0, j = 0;
+	/******************** petsc *********************/
+	Vec	T,QQ,b,x,w,t;
+	Mat	AA,BB;
+	KSP            ksp;    /*设置求解方法*/
+  	PC             pc;    /*设置求解参数*/
+	PetscErrorCode ierr;
+	PetscInt       n = 100, start = 0, end = n, num_of_nodes=(n+1)*(n+1);    /*这是将区域分成n块，start是起始边界，end是终止边界*/
+	PetscInt       i=0, ii = 0, j=0, col4[4], col6[6], col8[8], col9[9], rank;
+	PetscReal	h=0.01, q0=0;
+	PetscReal	k=50; /* time steps */
+	PetscScalar	value[9];
+	/******************** petsc *********************/
+	//double h=1;
+	//int i = 0, j = 0;
 	int length = 1;
-	int n = length / h;
+	//int n = length / h;
 	int num_of_elements = n * n;
-	int num_of_nodes = (n + 1) * (n + 1);
+	//int num_of_nodes = (n + 1) * (n + 1);
 	double nodes[num_of_nodes][3];
 	double q[4];
-	double T[num_of_nodes];
-	double Tdt[num_of_nodes];
+	//double T[num_of_nodes];
+	//double Tdt[num_of_nodes];
 	double B[4][4] = {{0.1111, 0.0556, 0.0278, 0.0556}, {0.0556, 0.1111, 0.0556, 0.0278}, {0.0278, 0.0556, 0.1111, 0.0556}, {0.0556, 0.0278, 0.0556, 0.1111}};
 	int elements[num_of_elements][5];
 	double A1[4][4] = {{0.6667, -0.1667, -0.3333, -0.1667}, {-0.1667, 0.6667, -0.1667, -0.3333}, {-0.3333, -0.1667, 0.6667, -0.1667}, {-0.1667, -0.3333, -0.1667, 0.6667}};
@@ -56,110 +68,184 @@ int main(/*int argc, char *argv[]*/)
 	B_matrix(B, h);
 	A_matrix(A, B, A1);
 	
-	/************single*****************/
-	printf("single A matrix:\n");
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			printf("%lf ",A[i][j]);
-		}
-		printf("\n");
-	}
-
-	printf("single Q matrix:\n");
-
-	for (int j = 0; j < 4; j++)
-	{
-		printf("%f \n",Q[j]);
-	}
-	printf("\n");
-
-
-	printf("single B matrix:\n");
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			printf("%lf ",B[i][j]);
-		}
-		printf("\n");
-	}
-
 	/***********global***************/
 	assemble_G_A(G_A,A,num_of_elements,num_of_nodes,elements);
-	printf("global A matrix:\n");
-	for (int i = 0; i < num_of_nodes; i++)
-	{
-		for (int j = 0; j < num_of_nodes; j++)
-		{
-			printf("%lf ",G_A[i][j]);
-		}
-		printf("\n");
-	}
-	
 	assembel_G_Q(G_Q,h,nodes,num_of_elements,num_of_nodes,elements);
-	printf("global Q matrix:\n");
-
-	for (int j = 0; j < num_of_nodes; j++)
-	{
-		printf("%lf ",G_Q[j]);
-	}
-	printf("\n");
-
 	assemble_G_q(G_q,num_of_nodes,n,h);
-	printf("global q matrix:\n");
-	for (int j = 0; j < num_of_nodes; j++)
-	{
-		printf("%lf ",G_q[j]);
-	}
-	printf("\n");
-
 	assemble_G_B(G_B,B,num_of_elements,num_of_nodes,elements);
-	printf("global B matrix:\n");
-	for (int i = 0; i < num_of_nodes; i++)
-	{
-		for (int j = 0; j < num_of_nodes; j++)
-		{
-			printf("%lf ",G_B[i][j]);
-		}
-		printf("\n");
-	}
 	huayifa_G_Qaq(G_Q,G_q, n, num_of_nodes);
-	printf("huayifa Q matrix:\n");
-
-	for (int j = 0; j < num_of_nodes; j++)
-	{
-		printf("%lf ",G_Q[j]);
-	}
-	printf("\n");
 	huayifa_G_A(G_A, num_of_elements, num_of_nodes, n, h);	
-	printf("huayifa A matrix:\n");
-
-	for (int i = 0; i < num_of_nodes; i++)
-	{
-		for (int j = 0; j < num_of_nodes; j++)
-		{
-			printf("%lf ",G_A[i][j]);
-		}
-		printf("\n");
-	}
 	huayifa_G_B(G_B, num_of_elements, num_of_nodes, n, h);	
-	printf("huayifa B matrix:\n");
+	/******************** petsc *********************/	
 
-	for (int i = 0; i < num_of_nodes; i++)
+  /*其中i,ii是矩阵和向量的角标，rstart和rend均为设置矩阵时需要的参数，nlocal和rank为程序并行化所需参数*/
+	ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;    /*初始化Petsc*/
+	ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank);CHKERRQ(ierr);    /*设置并行MPI参数*/
+	ierr = VecCreate(PETSC_COMM_WORLD,&T);CHKERRQ(ierr);    /*创建一个并行空间*/
+  	ierr = VecSetSizes(T,PETSC_DECIDE,num_of_nodes);CHKERRQ(ierr);    /*创建一个长度(n+1)*(n+1)的向量*/
+	ierr = VecSetFromOptions(T);CHKERRQ(ierr);    /*从选项数据库中配置向量*/	
+	ierr = VecSet(T,0);CHKERRQ(ierr);
+	ierr = VecDuplicate(T,&QQ);CHKERRQ(ierr);    /*将T的格式赋给Q*/
+  	ierr = VecDuplicate(T,&b);CHKERRQ(ierr);    /*将T的格式赋给b*/
+	ierr = VecDuplicate(T,&x);CHKERRQ(ierr);    /*将T的格式赋给x*/
+	ierr = VecDuplicate(T,&t);CHKERRQ(ierr);    /*将T的格式赋给t*/
+	ierr = VecDuplicate(T,&w);CHKERRQ(ierr);    /*将T的格式赋给t*/
+	ierr = VecAssemblyBegin(T);CHKERRQ(ierr);    
+  	ierr = VecAssemblyEnd(T);CHKERRQ(ierr);
+
+	ierr = VecCopy(T,b);CHKERRQ(ierr);
+	ierr = VecAssemblyBegin(b);CHKERRQ(ierr);    
+  	ierr = VecAssemblyEnd(b);CHKERRQ(ierr);
+
+	ierr = VecCopy(T,x);CHKERRQ(ierr);
+	ierr = VecAssemblyBegin(x);CHKERRQ(ierr);    
+  	ierr = VecAssemblyEnd(x);CHKERRQ(ierr);
+
+	ierr = VecCopy(T,t);CHKERRQ(ierr);
+	ierr = VecAssemblyBegin(t);CHKERRQ(ierr);    
+  	ierr = VecAssemblyEnd(t);CHKERRQ(ierr);
+
+	ierr = VecCopy(T,w);CHKERRQ(ierr);
+	ierr = VecAssemblyBegin(w);CHKERRQ(ierr);    
+  	ierr = VecAssemblyEnd(w);CHKERRQ(ierr);
+	
+	ierr = VecSet(QQ,0);CHKERRQ(ierr);
+	if(rank == 0)
 	{
-		for (int j = 0; j < num_of_nodes; j++)
-		{
-			printf("%lf ",G_B[i][j]);
-		}
-		printf("\n");
+		for(i=0;i<num_of_nodes;i++)
+			{
+				q0=G_Q[i];
+				ierr = VecSetValues(QQ, 1, &i, &q0, INSERT_VALUES);CHKERRQ(ierr);    /*将向量的对应位置的值进行修改*/
+			}
 	}
+	ierr = VecAssemblyBegin(QQ);CHKERRQ(ierr);    
+  	ierr = VecAssemblyEnd(QQ);CHKERRQ(ierr);
+	ierr = VecView(QQ,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+	
+	ierr = MatCreate(PETSC_COMM_WORLD,&AA);CHKERRQ(ierr);    /*在并行空间创建一个矩阵A*/
+  	ierr = MatSetSizes(AA,PETSC_DECIDE,PETSC_DECIDE,num_of_nodes,num_of_nodes);CHKERRQ(ierr);    /*设置矩阵的行数和列数*/
+ 	ierr = MatSetFromOptions(AA);CHKERRQ(ierr);    /*从选项数据库中配置矩阵*/
+  	ierr = MatSetUp(AA);CHKERRQ(ierr);    /*开始建立矩阵*/
+	for (ii=0;ii<num_of_nodes;ii++){
+		if (ii==0){		
+			value[0]=G_A[ii][ii]; value[1]=G_A[ii][ii+1]; value[2]=G_A[ii][ii+n+1]; value[3]=G_A[ii][ii+n+2];
+			col4[0]=ii; col4[1]=ii+1; col4[2]=ii+n+1; col4[3]=ii+n+2;
+			ierr   = MatSetValues(AA,1,&ii,4,col4,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii>0 && ii<n ){
+			value[0]=G_A[ii][ii-1]; value[1]=G_A[ii][ii]; value[2]=G_A[ii][ii+1]; value[3]=G_A[ii][ii+n]; value[4]=G_A[ii][ii+n+1]; value[5]=G_A[ii][ii+n+2];
+			col6[0]=ii-1; col6[1]=ii; col6[2]=ii+1; col6[3]=ii+n; col6[4]=ii+n+1;  col6[5]=ii+n+2;
+			ierr   = MatSetValues(AA,1,&ii,6,col6,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii==n+1 ){
+			value[0]=G_A[ii][ii-n-1]; value[1]=G_A[ii][ii-n]; value[2]=G_A[ii][ii-1]; value[3]=G_A[ii][ii];value[4]=G_A[ii][ii+1];value[5]=G_A[ii][ii+n]; value[6]=G_A[ii][ii+n+1];value[7]=G_A[ii][ii+n+2];
+			col8[0]=ii-n-1; col8[1]=ii-n; col8[2]=ii-1; col8[3]=ii; col8[4]=ii+1;  col8[5]=ii+n; col8[6]=ii+n+1;  col8[7]=ii+n+2;
+			ierr   = MatSetValues(AA,1,&ii,8,col8,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii>n+1 && ii<(n+1)*n-1 ){
+			value[0]=G_A[ii][ii-n-2];value[1]=G_A[ii][ii-n-1]; value[2]=G_A[ii][ii-n]; value[3]=G_A[ii][ii-1]; value[4]=G_A[ii][ii];value[5]=G_A[ii][ii+1];value[6]=G_A[ii][ii+n]; value[7]=G_A[ii][ii+n+1];value[8]=G_A[ii][ii+n+2];
+			col9[0]=ii-n-2; col9[1]=ii-n-1; col9[2]=ii-n; col9[3]=ii-1; col9[4]=ii; col9[5]=ii+1;  col9[6]=ii+n; col9[7]=ii+n+1;  col9[8]=ii+n+2;
+			ierr   = MatSetValues(AA,1,&ii,9,col9,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii==(n+1)*n-1 ){
+			value[0]=G_A[ii][ii-n-2];value[1]=G_A[ii][ii-n-1]; value[2]=G_A[ii][ii-n]; value[3]=G_A[ii][ii-1]; value[4]=G_A[ii][ii];value[5]=G_A[ii][ii+1];value[6]=G_A[ii][ii+n]; value[7]=G_A[ii][ii+n+1];
+			col8[0]=ii-n-2; col8[1]=ii-n-1; col8[2]=ii-n; col8[3]=ii-1; col8[4]=ii; col8[5]=ii+1;  col8[6]=ii+n; col8[7]=ii+n+1;
+			ierr   = MatSetValues(AA,1,&ii,8,col8,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii>(n+1)*n-1 && ii<(n+1)*(n+1)-1){
+			value[0]=G_A[ii][ii-n-2];value[1]=G_A[ii][ii-n-1]; value[2]=G_A[ii][ii-n]; value[3]=G_A[ii][ii-1]; value[4]=G_A[ii][ii];value[5]=G_A[ii][ii+1];
+			col6[0]=ii-n-2; col6[1]=ii-n-1; col6[2]=ii-n; col6[3]=ii-1; col6[4]=ii; col6[5]=ii+1;
+			ierr   = MatSetValues(AA,1,&ii,6,col6,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii== (n+1)*(n+1)-1){
+			value[0]=G_A[ii][ii-n-2];value[1]=G_A[ii][ii-n-1]; value[2]=G_A[ii][ii-1]; value[3]=G_A[ii][ii];
+			col4[0]=ii-n-2; col4[1]=ii-n-1; col4[2]=ii-1; col4[3]=ii;
+			ierr   = MatSetValues(AA,1,&ii,4,col4,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+	}
+	/* Assemble the A matrix */
+  	ierr = MatAssemblyBegin(AA,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);    /*通知其余并行块将矩阵统一*/
+  	ierr = MatAssemblyEnd(AA,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);    /*结束通知*/
+  	ierr = MatView(AA, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);    /*打印矩阵，检查是否出错*/
+
+	ierr = MatCreate(PETSC_COMM_WORLD,&BB);CHKERRQ(ierr);    /*在并行空间创建一个矩阵B*/
+  	ierr = MatSetSizes(BB,PETSC_DECIDE,PETSC_DECIDE,num_of_nodes,num_of_nodes);CHKERRQ(ierr);    /*设置矩阵的行数和列数*/
+ 	ierr = MatSetFromOptions(BB);CHKERRQ(ierr);    /*从选项数据库中配置矩阵*/
+  	ierr = MatSetUp(BB);CHKERRQ(ierr);    /*开始建立矩阵*/
+	for (ii=0;ii<num_of_nodes;ii++){
+		if (ii==0){		
+			value[0]=G_B[ii][ii]; value[1]=G_B[ii][ii+1]; value[2]=G_B[ii][ii+n+1]; value[3]=G_B[ii][ii+n+2];
+			col4[0]=ii; col4[1]=ii+1; col4[2]=ii+n+1; col4[3]=ii+n+2;
+			ierr   = MatSetValues(BB,1,&ii,4,col4,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii>0 && ii<n ){
+			value[0]=G_B[ii][ii-1]; value[1]=G_B[ii][ii]; value[2]=G_B[ii][ii+1]; value[3]=G_B[ii][ii+n];value[4]=G_B[ii][ii+n+1];value[5]=G_B[ii][ii+n+2];
+			col6[0]=ii-1; col6[1]=ii; col6[2]=ii+1; col6[3]=ii+n; col6[4]=ii+n+1;  col6[5]=ii+n+2;
+			ierr   = MatSetValues(BB,1,&ii,6,col6,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii==n+1 ){
+			value[0]=G_B[ii][ii-n-1]; value[1]=G_B[ii][ii-n]; value[2]=G_B[ii][ii-1]; value[3]=G_B[ii][ii];value[4]=G_B[ii][ii+1];value[5]=G_B[ii][ii+n]; value[6]=G_B[ii][ii+n+1];value[7]=G_B[ii][ii+n+2];
+			col8[0]=ii-n-1; col8[1]=ii-n; col8[2]=ii-1; col8[3]=ii; col8[4]=ii+1;  col8[5]=ii+n; col8[6]=ii+n+1;  col8[7]=ii+n+2;
+			ierr   = MatSetValues(BB,1,&ii,8,col8,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii>n+1 && ii<(n+1)*n-1 ){
+			value[0]=G_B[ii][ii-n-2];value[1]=G_B[ii][ii-n-1]; value[2]=G_B[ii][ii-n]; value[3]=G_B[ii][ii-1]; value[4]=G_B[ii][ii];value[5]=G_B[ii][ii+1];value[6]=G_B[ii][ii+n]; value[7]=G_B[ii][ii+n+1];value[8]=G_B[ii][ii+n+2];
+			col9[0]=ii-n-2; col9[1]=ii-n-1; col9[2]=ii-n; col9[3]=ii-1; col9[4]=ii; col9[5]=ii+1;  col9[6]=ii+n; col9[7]=ii+n+1;  col9[8]=ii+n+2;
+			ierr   = MatSetValues(BB,1,&ii,9,col9,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii==(n+1)*n-1 ){
+			value[0]=G_B[ii][ii-n-2];value[1]=G_B[ii][ii-n-1]; value[2]=G_B[ii][ii-n]; value[3]=G_B[ii][ii-1]; value[4]=G_B[ii][ii];value[5]=G_B[ii][ii+1];value[6]=G_B[ii][ii+n]; value[7]=G_B[ii][ii+n+1];
+			col8[0]=ii-n-2; col8[1]=ii-n-1; col8[2]=ii-n; col8[3]=ii-1; col8[4]=ii; col8[5]=ii+1;  col8[6]=ii+n; col8[7]=ii+n+1;
+			ierr   = MatSetValues(BB,1,&ii,8,col8,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii>(n+1)*n-1 && ii<(n+1)*(n+1)-1){
+			value[0]=G_B[ii][ii-n-2];value[1]=G_B[ii][ii-n-1]; value[2]=G_B[ii][ii-n]; value[3]=G_B[ii][ii-1]; value[4]=G_B[ii][ii];value[5]=G_B[ii][ii+1];
+			col6[0]=ii-n-2; col6[1]=ii-n-1; col6[2]=ii-n; col6[3]=ii-1; col6[4]=ii; col6[5]=ii+1;
+			ierr   = MatSetValues(BB,1,&ii,6,col6,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+		else if (ii== (n+1)*(n+1)-1){
+			value[0]=G_B[ii][ii-n-2];value[1]=G_B[ii][ii-n-1]; value[2]=G_B[ii][ii-1]; value[3]=G_B[ii][ii];
+			col4[0]=ii-n-2; col4[1]=ii-n-1; col4[2]=ii-1; col4[3]=ii;
+			ierr   = MatSetValues(BB,1,&ii,4,col4,value,INSERT_VALUES);CHKERRQ(ierr);
+		}
+	}
+	/* Assemble the B matrix */
+  	ierr = MatAssemblyBegin(BB,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);    /*通知其余并行块将矩阵统一*/
+  	ierr = MatAssemblyEnd(BB,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);    /*结束通知*/
+  	ierr = MatView(BB, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);    /*打印矩阵，检查是否出错*/
+	/* Q-A*T */
+	ierr = KSPCreate(PETSC_COMM_WORLD,&ksp);CHKERRQ(ierr);    /*创建ksp解空间*/
+  	ierr = KSPSetOperators(ksp,BB,BB);CHKERRQ(ierr);    /*设置方程左侧的系数*/
+ 	ierr = KSPGetPC(ksp,&pc);CHKERRQ(ierr);    /*设置矩阵求解的相关系数*/
+  	ierr = PCSetType(pc,PCJACOBI);CHKERRQ(ierr);    /*设置pc的默认参数*/
+  	ierr = KSPSetTolerances(ksp,1.e-10,PETSC_DEFAULT,PETSC_DEFAULT,PETSC_DEFAULT);CHKERRQ(ierr);    /*设置各种误差值*/
+  	ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);    /*从选项数据库中配置ksp解空间*/
+
+	while(PetscAbsReal(k)<51){
+		k +=1;
+		ierr = MatMult(AA,t,b);
+		ierr = VecWAXPY(w,-1,t,QQ);
+		ierr = KSPSolve(ksp,w,T);CHKERRQ(ierr);
+		ierr = VecCopy(T,t);CHKERRQ(ierr);    /*将T的值赋给t*/
+	}
+	ierr = VecView(T,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);    /*打印向量*/
+ 
+	ierr = VecDestroy(&T);CHKERRQ(ierr);    /*关闭向量T*/	
+	ierr = VecDestroy(&QQ);CHKERRQ(ierr);    /*关闭向量Q*/
+  	ierr = VecDestroy(&x);CHKERRQ(ierr);    /*关闭向量x*/
+  	ierr = VecDestroy(&t);CHKERRQ(ierr);    /*关闭向量t*/
+  	ierr = VecDestroy(&w);CHKERRQ(ierr);    /*关闭向量w*/
+  	ierr = VecDestroy(&b);CHKERRQ(ierr);    /*关闭向量b*/
+  	ierr = MatDestroy(&AA);CHKERRQ(ierr);    /*关闭矩阵A*/
+	ierr = MatDestroy(&BB);CHKERRQ(ierr);    /*关闭矩阵B*/
+
+ 	ierr = PetscFinalize();    /*结束并行*/
+	/*************************************** petsc end ***************************************/
 	// free matrices
 	freeMatrix(G_A);
 	freeMatrix(G_B);
 	free(G_Q);
-	return 0;
+	return ierr;
 }
 
 void nodes_matrix(double nodes[][3], int num_of_nodes, int n, double h)
@@ -359,7 +445,7 @@ void huayifa_G_Qaq(double *G_Q,double *G_q, int n, int num_of_nodes)
 	{
 		G_Q[i]=G_Q[i]-G_q[i];
 	}
-	for (i=0;i<n;i++)
+	for (i=0;i<n+1;i++)
 	{
 		G_Q[i]=0;
 	}
@@ -373,7 +459,7 @@ void huayifa_G_A(double **G_A, int num_of_elements, int num_of_nodes, int n, dou
 {
 	int i=0;
 	int j=0;
-	for (i=0;i<n;i++)
+	for (i=0;i<n+1;i++)
 	{	
 		for (j=0;j<num_of_nodes;j++)
 		{
@@ -387,7 +473,7 @@ void huayifa_G_A(double **G_A, int num_of_elements, int num_of_nodes, int n, dou
 			}
 		}	
 	}
-	for (i=0;i<n;i++)
+	for (i=0;i<n+1;i++)
 	{	
 		for (j=0;j<num_of_nodes;j++)
 		{
@@ -436,7 +522,7 @@ void huayifa_G_B(double **G_B, int num_of_elements, int num_of_nodes, int n, dou
 {
 	int i=0;
 	int j=0;
-	for (i=0;i<n;i++)
+	for (i=0;i<n+1;i++)
 	{	
 		for (j=0;j<num_of_nodes;j++)
 		{
@@ -450,7 +536,7 @@ void huayifa_G_B(double **G_B, int num_of_elements, int num_of_nodes, int n, dou
 			}
 		}	
 	}
-	for (i=0;i<n;i++)
+	for (i=0;i<n+1;i++)
 	{	
 		for (j=0;j<num_of_nodes;j++)
 		{
